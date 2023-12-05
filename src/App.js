e00cff01d989abd2d93396a7f5827a221fa03ee0
Madhusudhan Reddy <madhusudhanreddy@MadhusudhansMBP.lan>
2023-12-04 19:05:17 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css';
+import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">
+                        Personal Digital Assistants
+                    </p>
+                </div>
+            </section>
+
+            <div className='container'>
+                <div className='section'>
+                    <div class="columns">
+                        <div class="column is-4">
+                            <ProfileCard 
+                                title='alexa' 
+                                handle='@alexa99' 
+                                image={AlexaImage} 
+                                description='Alexa was made by Amazon and help you buy things'
+                            />
+                        </div>
+                        <div class="column is-4">
+                            <ProfileCard 
+                                title='cortana' 
+                                handle='@cortana' 
+                                image={CortanaImage}
+                                description='Alexa was made by microsfort and help you buy things'
+                            />
+                        </div>
+                        <div class="column is-4">
+                            <ProfileCard 
+                                title='siri' 
+                                handle='@siri'
+                                image={SiriImage}
+                                description='Alexa was made by Apple and help you buy things'
+                            />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default App;
